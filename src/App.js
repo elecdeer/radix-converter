832291710068bd66b6148a210c33de19e8c160ff
deer05 <deersub@gmail.com>
2019-11-04 02:03:46 +0900

    Form test

--- src/App.js
+++ src/App.js
@@ -3,8 +3,44 @@ import React, { Component } from 'react';
 class App extends Component {
   render() {
     return (
-    
-      <h1>Hello world</h1>
+      <FormApp />
+    );
+  }
+}
+
+class FormApp extends Component{
+  constructor(props){
+    super(props);
+
+    this.state = {
+      value: '',
+      message: ''
+    }
+  }
+
+  hundleInput({target: {value} }){
+    console.log('hundle input!')
+    this.setState({
+      value
+    });
+  }
+
+  send(){
+    console.log('button pushed!')
+    const {value} = this.state;
+    this.setState({
+      value: '',
+      message: value
+    })
+  }
+
+  render(){
+    return (
+      <div>
+        <input type="text" value={this.state.value} onChange={this.hundleInput.bind(this)}/>
+        <button onClick={this.send.bind(this)}>button</button>
+        <div>{this.state.message}</div>
+      </div>
     );
   }
 }
